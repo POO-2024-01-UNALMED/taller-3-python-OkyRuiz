708f166048d29467375ad1e669ccc98218a91760
OkyRuiz <159487334+OkyRuiz@users.noreply.github.com>
2024-02-26 11:18:55 -0500

    Intento 9

--- televisores/control.py
+++ televisores/control.py
@@ -1,8 +1,5 @@
 class Control:
 
-    def __init__(self, tv=None):
-        self._tv=tv 
-
     def enlazar(self, tv):
         self._tv= tv
         tv.setControl(self)
@@ -26,11 +23,9 @@ class Control:
         self._tv.volumenDown()
 
     def setVolumen(self, volumen):
-        if (volumen>=0 and volumen<=7):
             self._tv.setVolumen(volumen)
 
     def setCanal(self, canal):
-        if (canal>=1 and canal<=120):
             self._tv.setCanal(canal)
 
     #METODO SETTER Y GETTER TV
